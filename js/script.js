18afd6be3fd5aeab091353966815cf991ad9b4ba
Rajneesh Kumar Karna <rkkarna123@gmail.com>
2024-10-06 14:48:40 +0530

    add computerChoice function to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('It\'s working!');
+function computerChoice(computerChoice){
+  const randomNumber = Math.floor(Math.random() * 9);
+  if(randomNumber <= 2){
+    computerChoice = 'rock';
+  }
+  else if(randomNumber <= 5){
+    computerChoice = 'paper';
+  }
+  else{
+    computerChoice = 'scissors';
+  }
+  console.log(computerChoice);
+}
+computerChoice();
